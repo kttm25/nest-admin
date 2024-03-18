238e23863289bb15dfed1f4e10a23335045e4ad2
Mikhael KAMDEM <mikhaelkamdem45@gmail.com>
2024-03-18 09:22:54 +0100

    Add role module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
+import { RoleController } from './role/role.controller';
+import { RoleService } from './role/role.service';
+import { RoleModule } from './role/role.module';
 
 @Module({
   imports: [
@@ -21,9 +24,10 @@ import { CommonModule } from './common/common.module';
     }),
     AuthModule,
     CommonModule,
+    RoleModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, RoleController],
+  providers: [AppService, RoleService],
 })
 export class AppModule {}
 
