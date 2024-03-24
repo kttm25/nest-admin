ac60d5e3972bd8d83b2ee26881883b8d63dcb758
Mikhael KAMDEM <mikhaelkamdem45@gmail.com>
2024-03-25 00:01:26 +0100

    Update module controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { ProductCreateDto } from './models/product-create.dto'
+import { ProductUpdateDto } from './models/product-update.dto';
 
 @UseGuards(AuthGuard)
 @Controller('products')
@@ -11,11 +13,30 @@ export class ProductController {
 
     @Get()
     async all(@Query('page') page = 1){
-         this.productService.paginate()
+        return this.productService.paginate()
     }
 
     @Post()
     async create(@Body() body: ProductCreateDto){
-        this.productService.create(body)
+        return this.productService.create(body)
+    }
+
+    @Get(':id')
+    async get(@Param('id') id: number){
+        return this.productService.findOne({id})
+    }
+
+    @Put(':id')
+    async update(
+        @Param('id') id: number,
+        @Body() body: ProductUpdateDto
+    ){
+        await this.productService.update(id, body)
+        this.productService.findOne({id})
+    }
+
+    @Delete(':id')
+    async delete(@Param("id") id: number){
+        return this.productService.delete(id)
     }
 }
