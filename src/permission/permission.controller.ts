d5bf1b77a905f4d7857d9fb1e888b4be55ff5ac2
Mikhael KAMDEM <mikhaelkamdem45@gmail.com>
2024-03-26 18:29:51 +0100

    Add Custom decorator

--- src/permission/permission.controller.ts
+++ src/permission/permission.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { PermissionService } from './permission.service';
+import { HasPermission } from './has-permission.decorator';
 
 @Controller('permissions')
 export class PermissionController {
@@ -8,6 +9,7 @@ export class PermissionController {
     }
 
     @Get()
+    @HasPermission('view_permissions')
     async all(){
         return this.permissionService.all()
     }
